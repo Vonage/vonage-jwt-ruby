07f7b1a2d7912226de9e588ed821e19facb39ce7
superchilled <karl@superchilled.co.uk>
2023-10-25 16:02:51 +0100

    Implementing decode and verify_signature functionality

--- spec/vonage-jwt/jwt_spec.rb
+++ spec/vonage-jwt/jwt_spec.rb
@@ -4,16 +4,6 @@ describe Vonage::JWT do
   let(:token) { "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1ODc0OTQ5NjIsImp0aSI6ImM1YmE4ZjI0LTFhMTQtNGMxMC1iZmRmLTNmYmU4Y2U1MTFiNSIsImlzcyI6IlZvbmFnZSIsInBheWxvYWRfaGFzaCI6ImQ2YzBlNzRiNTg1N2RmMjBlM2I3ZTUxYjMwYzBjMmE0MGVjNzNhNzc4NzliNmYwNzRkZGM3YTIzMTdkZDAzMWIiLCJhcGlfa2V5IjoiYTFiMmMzZCIsImFwcGxpY2F0aW9uX2lkIjoiYWFhYWFhYWEtYmJiYi1jY2NjLWRkZGQtMDEyMzQ1Njc4OWFiIn0.JQRKi1d0SQitmjPINfTWMpt3XZkGsLbD7EjCdXoNSbk" }
 
   describe '.decode' do
-    let(:decoded_token) do
-      [{"iat"=>1587494962,
-        "jti"=>"c5ba8f24-1a14-4c10-bfdf-3fbe8ce511b5",
-        "iss"=>"Vonage",
-        "payload_hash"=>"d6c0e74b5857df20e3b7e51b30c0c2a40ec73a77879b6f074ddc7a2317dd031b",
-        "api_key"=>"a1b2c3d",
-        "application_id"=>"aaaaaaaa-bbbb-cccc-dddd-0123456789ab"},
-       {"alg"=>"HS256", "typ"=>"JWT"}]
-    end
-
     it 'correctly decodes the token payload' do
       expect(Vonage::JWT.decode(token, nil, false).first).to match(
         hash_including(
